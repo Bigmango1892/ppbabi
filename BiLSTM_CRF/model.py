1bbc4e94555370a2266516dbf49a35eb1a500a18
MarshBear <328553522@qq.com>
2022-06-21 15:03:39 +0800

    Update 6.21: Dimension Confirmed

--- BiLSTM_CRF/model.py
+++ BiLSTM_CRF/model.py
@@ -28,7 +28,10 @@ EPOCH = 100
 def argmax(vec):
     # return the argmax as a python int
     _, idx = torch.max(vec, 1)
-    return idx.item()
+    if idx.shape == torch.Size([1]):
+        return idx.item()
+    else:
+        return idx
 
 
 def prepare_sequence(seq, to_ix):
@@ -38,9 +41,9 @@ def prepare_sequence(seq, to_ix):
 
 # Compute log sum exp in a numerically stable way for the forward algorithm
 def log_sum_exp(vec):
-    max_score = vec[0, argmax(vec)]
-    max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
-    return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+    max_score = vec[range(BATCH_SIZE), argmax(vec)]
+    max_score_broadcast = max_score.view(-1, 1).expand(BATCH_SIZE, vec.size()[1])
+    return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast), 1))
 
 def batch_length_pad(sentences):
     pad_datas = pad_sequence(sentences, batch_first=True, padding_value=0)
@@ -210,7 +213,7 @@ class BiLSTM_CRF(nn.Module):
         self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, num_layers=1, bidirectional=True)
 
         # Maps the output of the LSTM into tag space.
-        self.hidden2tag = nn.Linear(hidden_dim+3, self.tagset_size)
+        self.hidden2tag = nn.Linear(2 * hidden_dim + 3, self.tagset_size)
 
         # Matrix of transition parameters.  Entry i,j is the score of
         # transitioning *to* i *from* j.
@@ -241,20 +244,24 @@ class BiLSTM_CRF(nn.Module):
         lstm_out, self.hidden = self.lstm(embeds, self.hidden)
         return lstm_out, embeds
 
-    def _feat_splice(self, sentence, embeds):
+    def _feat_splice(self, sentence, lstm_feats, embeds):
         sentence_pos = batch_length_pad([t.pos for t in sentence])
         sentence_con = batch_length_pad([t.con for t in sentence])
         sentence_seg = batch_length_pad([t.seg for t in sentence])
-        eig = torch.cat((embeds, sentence_pos.view(-1, len(sentence), 1), sentence_con.view(-1, len(sentence), 1), sentence_seg.view(-1,len(sentence), 1)),2)
-        split_feat = self.hidden2tag(eig)
+        lstm_feats_split, _ = pad_packed_sequence(lstm_feats, batch_first=True)
+        lstm_feats_split = lstm_feats_split.transpose(0, 1)
+        eig = torch.cat((lstm_feats_split, embeds, sentence_pos.view(-1, len(sentence), 1), sentence_con.view(-1, len(sentence), 1), sentence_seg.view(-1,len(sentence), 1)),2)
+        hidden_feat = self.hidden2tag(eig)
+        split_feat = torch.tanh(hidden_feat)
         return split_feat
 
     def _forward_alg(self, feats):
         # TODO 查找CRF实现
         # Do the forward algorithm to compute the partition function
-        init_alphas = torch.full((1, self.tagset_size), -10000.)
+        init_alphas = torch.full((BATCH_SIZE, self.tagset_size), -10000.)
         # START_TAG has all the score.
-        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+        for i in range(BATCH_SIZE):
+            init_alphas[i][self.tag_to_ix[START_TAG]] = 0.
 
         # Wrap in a variable so that we will get automatic backprop
         forward_var = init_alphas
@@ -264,25 +271,27 @@ class BiLSTM_CRF(nn.Module):
             alphas_t = []  # The forward tensors at this timestep
             for next_tag in range(self.tagset_size):
                 # broadcast the emission score: it is the same regardless of the previous tag
-                emit_score = feat[next_tag].view(1, -1).expand(1, self.tagset_size)
+                emit_score = feat[:, next_tag].view(-1, 1).expand(BATCH_SIZE, self.tagset_size)
                 # the i-th entry of trans_score is the score of transitioning to next_tag from i
-                trans_score = self.transitions[next_tag].view(1, -1)
+                trans_score = self.transitions[next_tag].view(1, -1).expand(BATCH_SIZE, self.tagset_size)
                 # The i-th entry of next_tag_var is the value for the edge (i -> next_tag) before we do log-sum-exp
                 next_tag_var = forward_var + trans_score + emit_score
                 # The forward variable for this tag is log-sum-exp of all the scores.
-                alphas_t.append(log_sum_exp(next_tag_var).view(1))
-            forward_var = torch.cat(alphas_t).view(1, -1)
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+                alphas_t.append(log_sum_exp(next_tag_var).view(-1, 1))
+            forward_var = torch.cat(alphas_t, 1)
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]].expand(BATCH_SIZE, self.tagset_size)
         alpha = log_sum_exp(terminal_var)
         return alpha
 
     def _score_sentence(self, feats, tags):
         # Gives the score of a provided tag sequence
-        score = torch.zeros(1)
-        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+        score = torch.zeros(BATCH_SIZE)
+        tags = [torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tag,
+                           self.tag_to_ix['O'] * torch.ones(feats.size()[0]-tag.size()[0])]).view(1, -1) for tag in tags]
+        tags = torch.cat(tags).long()
         for i, feat in enumerate(feats):
-            score = score + self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
-        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+            score = score + self.transitions[tags[:, i + 1], tags[:, i]] + feat[range(BATCH_SIZE), tags[:, i + 1]]
+        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[:, -1]]
         return score
 
     def loss_function(self, sentence, tags):
@@ -290,12 +299,12 @@ class BiLSTM_CRF(nn.Module):
         sentence_onehot = [t.onehot for t in sentence]
         # sentence_onehot = batch_pack_padded(sentence_onehot_list, sentence_onehot)
         lstm_feats, embeds = self._get_lstm_features(sentence_onehot)
-        embeds,_ = pad_packed_sequence(embeds, batch_first= True)
-        embeds = embeds.transpose(0,1)
-        splice_feats = self._feat_splice(sentence, embeds)
+        embeds, _ = pad_packed_sequence(embeds, batch_first=True)
+        embeds = embeds.transpose(0, 1)
+        splice_feats = self._feat_splice(sentence, lstm_feats, embeds)
         forward_score = self._forward_alg(splice_feats)
         gold_score = self._score_sentence(splice_feats, tags)
-        return forward_score - gold_score
+        return torch.sum(forward_score - gold_score)
     # TODO 添加其他特征值进入模型
 
     def _viterbi_decode(self, feats):
