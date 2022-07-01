c6f234ee81eac5821b54e02a1d56eee052bac157
MarshPaperball <328553522@qq.com>
2022-07-01 17:29:02 +0800

    JD cate

--- JD_category/output.py
+++ JD_category/output.py
@@ -2,8 +2,8 @@ import pandas as pd
 import openpyxl as op
 import pickle
 
-category_path = '/Users/dingyouqian/仓库/PaperBall/Zhilian_Data/ppbcate/renew/分类目录.xlsx'
-JD_path = '/Users/dingyouqian/仓库/PaperBall/ppbabi/ppbabi/renew/JD_nona.csv'
+category_path = 'D:/Codes/Zhilian_Data/ppbcate/renew/分类目录.xlsx'
+JD_path = 'D:/Codes/ppbabi/ppbabi/renew/jd_nona.csv'
 
 ws = op.load_workbook(category_path)['Sheet2']
 category = [ws.cell(i, j).value for i in range(3, 110) for j in range(2, 104) if ws.cell(i, j).value]
@@ -16,6 +16,13 @@ for i in range(len(JD)):
         if category[j] in JD.loc[i, '岗位具体名称']:
             index[j].append(i)
             break
+    if i % 1000 == 0:
+        print(i)
+
+index_nona = {}
+for i in range(len(index)):
+    if index[i]:
+        index_nona[category[i]] = index[i]
 
 with open('JD岗位分类.data', 'wb') as f:
-    pickle.dump(index, f)
+    pickle.dump(index_nona, f)
